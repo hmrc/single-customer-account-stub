a120e9ab7224617affd9bc3b33061f0e94f71360
Yang Wong <20066247+yangwong9664@users.noreply.github.com>
2023-03-22 16:15:25 +0000

    update dependencies

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -8,10 +8,12 @@ object AppDependencies {
   val compile = Seq(
     "uk.gov.hmrc"       %% "url-builder"                % "3.8.0-play-28",
     "uk.gov.hmrc"             %% "bootstrap-backend-play-28"  % "7.15.0",
-    "uk.gov.hmrc"         %% "play-partials"                % "8.4.0-play-28"
+    "uk.gov.hmrc"         %% "play-partials"                % "8.4.0-play-28",
+    "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-28" % "0.70.0"
   )
 
   val test = Seq(
-    "uk.gov.hmrc"             %% "bootstrap-test-play-28"     % "7.15.0"             % "test, it"
+    "uk.gov.hmrc"             %% "bootstrap-test-play-28"     % "7.15.0"             % "test, it",
+    "uk.gov.hmrc.mongo" %% "hmrc-mongo-test-play-28" % "0.70.0" % Test
   )
 }
