4288e7b4180858311eec46db897ee38652a66d25
MrSleeth <5717589+MrSleeth@users.noreply.github.com>
2025-09-22 16:57:01 +0100

    Update dependencies from platops-pr-bot

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -7,7 +7,7 @@ object AppDependencies {
 
   val compile: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"             %% s"bootstrap-backend-$playVersion"    % bootstrapPlayVersion,
-    "uk.gov.hmrc"             %% s"play-partials-$playVersion"        % "10.0.0"
+    "uk.gov.hmrc"             %% s"play-partials-$playVersion"        % "10.1.0"
   )
 
   val test: Seq[ModuleID] = Seq(
